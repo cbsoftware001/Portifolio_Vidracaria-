2426f0f8e2353627bdaab97e8ed4cd102ecc1a33
Caique B. CbSoftWare <desenvolvimentoweb123@gmail.com>
2019-09-04 12:46:56 -0300

    Criando Arquitetura d Código Base do Sistema

---
+++ Template/Header.php
@@ -0,0 +1,24 @@
+<!-- iniciando o documento html -->
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <title>Portifolio Vidraçaria</title>
+    <meta chasert="utf-8" />
+    <meta name="viewport" content="width=device-width, user-scaleble=1" />
+    <meta name="author" content="Caique Bispo - CbSoftWare" />
+    <meta name="Description" content="Vidraçaria | Vidros, Dicas, Atacado, Varejo, Decoração, Arquiterura | Divinal" />
+    <meta name="keywords" content="Vidros, Portas" />
+    <meta property="og:locale" content="pt_BR" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Vidraçaria | Vidros, Dicas, Atacado, Varejo, Decoração, Arquiterura | Divinal" />
+    <meta property="og:description" content="Vidraçaria | Vidros, Dicas, Atacado, Varejo, Decoração, Arquiterura | Divinal" />
+    <meta property="og:url" content="" />
+
+    <!-- links -->
+    <link rel="stylesheet" type="text/css" href="Assets/Css/Style.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css" integrity="sha384-i1LQnF23gykqWXg6jxC2ZbCbUMxyw5gLZY6UiUS98LYV5unm8GWmfkIS6jqJfb4E" crossorigin="anonymous">
+
+    <!-- Script -->
+    <script type="text/javascript" src="Assets/Js/Script.js"></script>
+</head>
+<body>
