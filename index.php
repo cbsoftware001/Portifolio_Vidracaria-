a806b72ab45ad8774493e7dac5091717be0859d5
Caique B. CbSoftWare <desenvolvimentoweb123@gmail.com>
2019-09-05 13:15:25 -0300

    [INSERT] Criação da Area de Service

--- index.php
+++ index.php
@@ -17,10 +17,93 @@
             Estamos no Mercado as mais de 15 anos, com profissionais especializados.
             Atendemos em todas as regiões.
         </p>
-            <a href="">saiba mais -></a>
+            <a href="">saiba mais</a>
         </article>
     </div>
 </section>
 <!-- Criando a Section Service -->
+<section class="service">
+    <div class="container">
+        <div class="widget-service">
+            <div class="widget-service-title">
+                Janelas e Portas de Aluminios
+            </div>
+            <div class="widget-service-img">
+                <img src="Assets/Images/service01.jpg" alt="">
+            </div>
+            <div class="widget-service-description">
+                Fabricação e Instalação de Janelas e Portas de Aluminios
+                <br><br><br>
+                <a href="">Saiba Mais</a>
+            </div>
+        </div>
+        <div class="widget-service">
+            <div class="widget-service-title">
+                Espelhos
+            </div>
+            <div class="widget-service-img">
+                <img src="Assets/Images/service02.jpg" alt="">
+            </div>
+            <div class="widget-service-description">
+                Espelhos Lapidados, Bisõtes, Decorativos...
+                <br><br><br>
+                <a href="">Saiba Mais</a>
+            </div>
+        </div>
+        <div class="widget-service">
+            <div class="widget-service-title">
+                Guarda-Corpo
+            </div>
+            <div class="widget-service-img">
+                <img src="Assets/Images/service03.jpg" alt="">
+            </div>
+            <div class="widget-service-description">
+                Guarda Corpo de Vidro e Alumínio
+                <br><br><br>
+                <a href="">Saiba Mais</a>
+            </div>
+        </div>
+        <div class="widget-service">
+            <div class="widget-service-title">
+                Box para Banheiro
+            </div>
+            <div class="widget-service-img">
+                <img src="Assets/Images/service04.jpg" alt="">
+            </div>
+            <div class="widget-service-description">
+                Box Padrão, Box Elegance, Box de Canto...
+                <br><br><br>
+                <a href="">Saiba Mais</a>
+            </div>
+        </div>
+        <div class="widget-service">
+            <div class="widget-service-title">
+                Fechamento de Áreas e Sacadas
+            </div>
+            <div class="widget-service-img">
+                <img src="Assets/Images/service05.jpg" alt="">
+            </div>
+            <div class="widget-service-description">
+                Envidraçamentos de Sacadas, Áreas, Varandas...
+                <br><br><br>
+                <a href="">Saiba Mais</a>
+            </div>
+        </div>
+        <div class="widget-service">
+            <div class="widget-service-title">
+                Corrimão em Alumínio
+            </div>
+            <div class="widget-service-img">
+                <img src="Assets/Images/service06.jpg" alt="">
+            </div>
+            <div class="widget-service-description">
+                 Corrimão em Alumínio
+                <br><br><br>
+                <a href="">Saiba Mais</a>
+            </div>
+        </div>
+        <article><a href="" class="btn-saibaMais">saiba mais</a></article>
+    </div>
+</section>
 <!-- Requisitando Roda-pé da Pagina -->
 <?php require_once 'Template/Footer.php'; ?>
