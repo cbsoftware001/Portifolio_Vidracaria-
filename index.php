c05151b6b948242b9edd37773d387e3a3b5f3fbb
Caique B. CbSoftWare <desenvolvimentoweb123@gmail.com>
2019-09-04 21:49:04 -0300

    [INSERT] Criando a Area de Banner

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <!-- Requisitando Cabeçalho da Pagina -->
 <?php require_once 'Template/Header.php'; ?>
-
+<!-- Criando a Section Banner -->
+<section class="banner">
+    <div class="container">
+        <article>
+            <p>Vidraçaria / Portas e Janelas de Vidros</p>
+        </article>
+    </div>
+</section>
 <!-- Requisitando Roda-pé da Pagina -->
 <?php require_once 'Template/Footer.php'; ?>
