b6cf65f12539a559dc9adeca91fd339515a4918e
Caique B. CbSoftWare <desenvolvimentoweb123@gmail.com>
2019-09-13 18:14:45 -0300

    [INSERT] Finalziando a Pagina Galeria

--- galeria.php
+++ galeria.php
@@ -12,5 +12,101 @@
         </div>
     </div>
 </section>
+<!-- Craindo a Section Description Page -->
+<section class="description-page">
+    <div class="container">
+        <article>
+            <h1>Modelos de Projetos e Aplicações com Vidro e Alumínio</h1>
+            <p>
+                Esquadrias de Alumínio, Janelas e Portas de Vidro, Box de Banheiro, Vidros Temperado e Laminado, Guarda-Corpo e Corrimão, Espelhos, Envidraçamento de Sacadas <br>
+                Clique nas fotos para amplia-las.
+            </p>
+        </article>
+    </div>
+</section>
+<section class="photo-galerry">
+    <div class="container">
+       <div class="content-img">
+           <img src="Assets/Images/galeria01.jpg" alt="">
+           <p>Vidros Temperados e Laminados </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria02.jpg" alt="">
+           <p>Pisos e Cobertura de Vidro  </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria03.jpg" alt="">
+           <p>Box com Roldanas Aparente </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria04.jpg" alt="">
+           <p>Box de Canto </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria05.jpg" alt="">
+           <p>Box de Vidro </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria06.jpg" alt="">
+           <p>Box de Banheiro </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria07.jpg" alt="">
+           <p>Guarda-Corpo </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria08.jpg" alt="">
+           <p>Envidraçamento de Sacada</p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria09.jpg" alt="">
+           <p>Espelho para Banheiro </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria10.jpg" alt="">
+           <p>Espelho Bisotado </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria11.jpg" alt="">
+           <p>Espelho para Sala </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria12.jpg" alt="">
+           <p>Esquadrias de Alumínio </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria13.jpg" alt="">
+           <p>Esquadria em Alumínio </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria14.jpg" alt="">
+           <p>Guarda-Corpo e Esquadrias </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria15.jpg" alt="">
+           <p>Guarda-Corpo em Vidro </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria20.jpg" alt="">
+           <p>Portas de Vidro de Correr  </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria16.jpg" alt="">
+           <p>Janelas de Vidro </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria17.jpg" alt="">
+           <p>Janelas de Vidro Maxim-ar </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria18.jpg" alt="">
+           <p>Janelas de Vidro Perfis de Alumínio </p>
+       </div>
+       <div class="content-img">
+           <img src="Assets/Images/galeria19.jpg" alt="">
+           <p>Portas de Vidro de Correr  </p>
+       </div>
+    </div>
+</section>
 <!-- Requisitando Roda-pé da Pagina -->
 <?php require_once 'Template/Footer.php'; ?>
