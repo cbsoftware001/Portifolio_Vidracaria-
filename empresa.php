1e075565b7b2576f6630605421722860b4463dfb
Caique B. CbSoftWare <desenvolvimentoweb123@gmail.com>
2019-09-14 19:36:02 -0300

    [CREATE] Section Info Empresa, da Pagina Empresa

--- empresa.php
+++ empresa.php
@@ -12,5 +12,39 @@
         </div>
     </div>
 </section>
+<!-- Section Description Empresa -->
+<section class="description_empresa">
+    <div class="container">
+        <article>
+            <h1>A Sua Melhor Opção em Vidraçaria e Alumínio</h1>
+        </article>
+        <section class="sobre_empresa">
+            <article>
+                <div class="title">
+                    Nossa Empresa
+                </div>
+                <div class="commit">
+                Somos uma empresa especializada em vidraçaria e alumínio. Trabalhamos com profissionais com mais de 15 anos no mercado, especializados no desenvolvimento do produto até a instalação do vidro ou alumínio. A Vidraçaria Pinhais é uma empresa dinâmica, atuando na área de vidros temperados e laminados, esquadrias de alumínio, espelhos e bisotê entre outros serviços, buscando incessantemente a excelência ao produto acabado sempre oferecendo qualidade elevada a um preço justo.
+                </div>
+            </article>
+            <article>
+                <div class="title">
+                    Nossa Missão
+                </div>
+                <div class="commit">
+                Nossa missão é levar produtos diferenciados e modernos em projetos com vidro, espelho e alumínio para as residências e empresas de nossa região. Investir em produtos de qualidade para o seu patrimônio é fundamental. Por isso a Vidraçaria Pinhais se destaca em inúmeros projetos, por prezar a alta qualidade e sofisticação dos serviços prestados a seus clientes.
+                </div>
+            </article>
+            <article>
+                <div class="title">
+                    Nossos Valores
+                </div>
+                <div class="commit">
+                Nosso principal foco é sempre prestar um serviço de qualidade e satisfatório, oferecendo produtos com alta durabilidade e que proporcionem conforto e bem-estar para a sua casa ou escritório. Respeito, atendimento diferenciado alinhado ao um orçamento justo.
+                </div>
+            </article>
+        </section>
+    </div>
+</section>
 <!-- Requisitando Roda-pé da Pagina -->
 <?php require_once 'Template/Footer.php'; ?>
